5f1ebe002d3b77b77101810140eaf02d0e1a912f
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-11 12:58:54 +0600

    basic mvc

---
+++ 01-Intro/Controllers/ItemsController.cs
@@ -0,0 +1,13 @@
+using _01_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Intro.Controllers
+{
+    public class ItemsController : Controller {
+        public IActionResult Overview()
+        {
+            Item item = new Item(){Name = "Keyboard"};
+            return View(item);
+        }
+    }
+}
---
+++ 01-Intro/Models/Item.cs
@@ -0,0 +1,8 @@
+namespace _01_Intro.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
