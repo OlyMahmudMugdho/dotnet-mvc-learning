b8d2af34322bf62d74387b20a0f1571a1d531418
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-14 21:46:57 +0600

    model

---
+++ StudentCRUD/Models/Student.cs
@@ -0,0 +1,8 @@
+namespace StudentCRUD.Models;
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
