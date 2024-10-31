33138208c621aa212bbe32c5559397a5a956f17d
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-01 01:48:59 +0600

    update/edit employee

--- EmployeeCrud/EmployeeCrud/Controllers/EmployeeController.cs
+++ EmployeeCrud/EmployeeCrud/Controllers/EmployeeController.cs
@@ -6,22 +6,23 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 namespace EmployeeCrud;
 
 public class EmployeeController : Controller
-{   
+{
     private readonly AppDbContext _context;
 
-    public EmployeeController(AppDbContext context) {
+    public EmployeeController(AppDbContext context)
+    {
         this._context = context;
     }
 
-    
+
     public async Task<IActionResult> Index()
-    {   
+    {
         Task<List<Employee>> employees = _context.employees.ToListAsync();
         return View(await employees);
     }
 
     public async Task<IActionResult> Details(int? id)
-    {   
+    {
 
         if (id == null)
         {
@@ -33,13 +34,15 @@ public class EmployeeController : Controller
     }
 
     [HttpGet]
-    public IActionResult Add(){
+    public IActionResult Add()
+    {
         Employee employee = new Employee();
         return View(employee);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Add([Bind("Id,Name,Department")] Employee employee){
+    public async Task<IActionResult> Add([Bind("Id,Name,Department")] Employee employee)
+    {
         if (ModelState.IsValid)
         {
             _context.Add(employee);
@@ -47,5 +50,38 @@ public class EmployeeController : Controller
             return RedirectToAction(nameof(Index));
         }
         return View(employee);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        Task<Employee> employee = _context.employees.FirstAsync(m => m.Id == id);
+
+        return View(await employee);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit([Bind("Id,Name,Department")] Employee employee)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(employee);
+        }
+
+        try
+        {
+            _context.employees.Update(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        catch (System.Exception)
+        {
+            return View(employee);
+        }
     }
 }
--- EmployeeCrud/EmployeeCrud/Models/Employee.cs
+++ EmployeeCrud/EmployeeCrud/Models/Employee.cs
@@ -1,3 +1,4 @@
+
 namespace EmployeeCrud.Models;
 
 public class Employee
@@ -5,4 +6,9 @@ public class Employee
     public int Id{get;set;}
     public string Name { get; set; }
     public string Department{get;set;}
+
+    public static implicit operator Task<object>(Employee? v)
+    {
+        throw new NotImplementedException();
+    }
 }
